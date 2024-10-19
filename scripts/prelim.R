7919373abf9c2089aee73947f1993ce1dbe8bb0f
ArguablyOkay <pmallaney1@gmail.com>
2024-10-19 01:25:09 +0100

    Committing first files, getting started

---
+++ scripts/prelim.R
@@ -0,0 +1,14 @@
+## SETUP ----
+# Necessary Packages 
+library(tidyverse)
+library(openxlsx)
+
+# Read in data, some cleanup for personal preference
+monsters <- read.xlsx("data/5e_monsters.xlsx", "Official Stats") %>% 
+  rename_with(tolower) %>% 
+  arrange(cr, type, name) %>% 
+  mutate(speeds = if_else(is_number(substr(speeds, 1, 1)), # Condition
+                          paste0("walk ", speeds), # If True
+                          speeds)) # If False
+
+## 
